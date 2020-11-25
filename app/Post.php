1498f91ad73776737f424de9371798dc7989d8c3
Charles <c.awulor@yahoo.com>
2020-11-25 09:28:07 +0100

    'added'

---
+++ app/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //table name
+   // protected $table = 'posts';
+    //primary key
+    //public $primarykey = 'id';
+
+    //timestamps
+    //public $timestamps = false;
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+        <h1>{{$title}}</h1>   
+        <p>this is a laravel tutorial series</p> 
+   
+ @endsection
+
